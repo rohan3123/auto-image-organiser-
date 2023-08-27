981e1b2f334f1a3555d924120adea120c0d991a0
Rohan <rohangazi30@gmail.com>
2023-08-27 15:44:29 +0100

    list all the files in downloads folder

--- autoimage.py
+++ autoimage.py
@@ -8,3 +8,9 @@ meme_folder = "C:\Users\false\Downloads\meme"
 # Ensure the meme folder exists
 if not os.path.exists(meme_folder):
     os.makedirs(meme_folder)
+    
+# List all files in the downloads folder
+downloaded_files = os.listdir(downloads_folder)
+
+# List of image file extensions
+image_extensions = ['.jpg', '.jpeg', '.png', '.gif']
