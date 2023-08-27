f0d0af34161feb685de48d9d0975a65e054d5d62
Rohan <rohangazi30@gmail.com>
2023-08-27 15:43:35 +0100

    create meme folder if it doesnt exist

--- autoimage.py
+++ autoimage.py
@@ -2,4 +2,9 @@ import os
 import shutil
 
 # Path to your Downloads folder
-downloads_folder = os.path.expanduser("~/Downloads")
+downloads_folder = "C:\Users\false\Downloads"
+meme_folder = "C:\Users\false\Downloads\meme"
+
+# Ensure the meme folder exists
+if not os.path.exists(meme_folder):
+    os.makedirs(meme_folder)
