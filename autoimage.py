b8dd4ae49e295471a3581542c20c604db9c0f49e
Rohan <rohangazi30@gmail.com>
2023-08-27 15:45:15 +0100

    created a for loop to move files

--- autoimage.py
+++ autoimage.py
@@ -14,3 +14,18 @@ downloaded_files = os.listdir(downloads_folder)
 
 # List of image file extensions
 image_extensions = ['.jpg', '.jpeg', '.png', '.gif']
+
+# Move image files to the meme folder
+for filename in downloaded_files:
+    if any(filename.lower().endswith(ext) for ext in image_extensions):
+        source_path = os.path.join(downloads_folder, filename)
+        destination_path = os.path.join(meme_folder, filename)
+        
+        # Check if the file already exists in the destination folder
+        if os.path.exists(destination_path):
+            print(f"File '{filename}' already exists in the meme folder.")
+        else:
+            shutil.move(source_path, destination_path)
+            print(f"Moved '{filename}' to meme folder.")
+
+print("Image moving process completed!")
